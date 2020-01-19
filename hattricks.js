16880b5f1ec30eb9d0db920e470b3e613c6d74f9
brad604 <59180282+brad604@users.noreply.github.com>
2020-01-19 11:30:52 -0800

    added new record JSs

---
+++ hattricks.js
@@ -0,0 +1,19 @@
+
+$.getJSON("https://records.nhl.com/site/api/three-or-more-goal-games-career?cayenneExp=threeOrMoreGoalGames>=1%20and%20playerId=8471214", function(data){
+	
+	console.log(data);
+	
+	
+
+	var hats = data.data[0].threeOrMoreGoalGames;
+
+	
+	
+
+	
+
+		$('.hats').append(hats);
+
+	
+	}
+		 );
---
+++ overtime.js
@@ -0,0 +1,19 @@
+
+$.getJSON("https://records.nhl.com/site/api/skater-career-scoring-regular-season?cayenneExp=playerId=8471214", function(data){
+	
+	console.log(data);
+	
+	
+
+	var otpoints = data.data[0].overtimePoints;
+
+	var otgoals = data.data[0].overtimeGoals;	
+	
+
+	
+
+		$('.otpoints').append(otpoints);
+		$('.otgoals').append(otgoals);
+	
+	}
+		 );
