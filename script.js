711c2bf477555820b06ab4cf33f21fb8da4ecd5d
brad604 <59180282+brad604@users.noreply.github.com>
2020-01-19 15:57:08 -0800

    more

--- script.js
+++ script.js
@@ -56,18 +56,5 @@ $.getJSON("https://statsapi.web.nhl.com/api/v1/people/8466139/stats?stats=career
 		 );
 
 
-$.getJSON("https://records.nhl.com/site/api/skater-career-scoring-regular-season?cayenneExp=playerId=8471214", function(data){
-	
-	console.log(data);
-	
 
-	var otpoints = data.data[0].overtimePoints;
-	var otgoals = data.data[0].overtimeGoals;	
-	
-
-		$('.otpoints').append(otpoints);
-		$('.otgoals').append(otgoals);
-	
-	}
-		 );
 
