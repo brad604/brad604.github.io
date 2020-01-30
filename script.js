dcef826b109b93d6c5ec36dd3582ed916a877cd4
brad604 <59180282+brad604@users.noreply.github.com>
2020-01-29 16:46:51 -0800

    swap JSs

--- script.js
+++ script.js
@@ -1,37 +1,4 @@
 
-$.getJSON("https://statsapi.web.nhl.com/api/v1/people/8471214/stats?stats=careerRegularSeason", function(data){
-	
-	console.log(data);
-	
-	var goals = data.stats[0].splits[0].stat.goals;
-	var goalsbehind = 894 - data.stats[0].splits[0].stat.goals + 1;
-	var ppgoals = data.stats[0].splits[0].stat.powerPlayGoals;
-	var ppgoalsbehind = 274 - data.stats[0].splits[0].stat.powerPlayGoals + 1;
-	var gwgoals = data.stats[0].splits[0].stat.gameWinningGoals;
-	var gwgoalsbehind = 135 - data.stats[0].splits[0].stat.gameWinningGoals + 1;
-	
-		$('.goals').append(goals);
- 		$('.goalsbehind').append(goalsbehind);
-		$('.ppgoals').append(ppgoals);
- 		$('.ppgoalsbehind').append(ppgoalsbehind);
-		$('.gwgoals').append(gwgoals);
- 		$('.gwgoalsbehind').append(gwgoalsbehind);	
-	
-	}
-		 );
-
-
-
-$.getJSON("https://statsapi.web.nhl.com/api/v1/people/8466139/stats?stats=careerRegularSeason", function(data){
-	
-	console.log(data);
-	
-	var pmgwgoals = data.stats[0].splits[0].stat.gameWinningGoals;
-
-		$('.pmgwgoals').append(pmgwgoals);
-	
-	}
-		 );
 
 $.getJSON("https://ovirecords.herokuapp.com/https://records.nhl.com/site/api/skater-career-scoring-regular-season?cayenneExp=playerId=8471214", 
 		  function(data){
@@ -131,3 +98,38 @@ $.getJSON("https://ovirecords.herokuapp.com/https://records.nhl.com/site/api/ska
  
     }
          );
+
+
+$.getJSON("https://statsapi.web.nhl.com/api/v1/people/8471214/stats?stats=careerRegularSeason", function(data){
+	
+	console.log(data);
+	
+	var goals = data.stats[0].splits[0].stat.goals;
+	var goalsbehind = 894 - data.stats[0].splits[0].stat.goals + 1;
+	var ppgoals = data.stats[0].splits[0].stat.powerPlayGoals;
+	var ppgoalsbehind = 274 - data.stats[0].splits[0].stat.powerPlayGoals + 1;
+	var gwgoals = data.stats[0].splits[0].stat.gameWinningGoals;
+	var gwgoalsbehind = 135 - data.stats[0].splits[0].stat.gameWinningGoals + 1;
+	
+		$('.goals').append(goals);
+ 		$('.goalsbehind').append(goalsbehind);
+		$('.ppgoals').append(ppgoals);
+ 		$('.ppgoalsbehind').append(ppgoalsbehind);
+		$('.gwgoals').append(gwgoals);
+ 		$('.gwgoalsbehind').append(gwgoalsbehind);	
+	
+	}
+		 );
+
+
+
+$.getJSON("https://statsapi.web.nhl.com/api/v1/people/8466139/stats?stats=careerRegularSeason", function(data){
+	
+	console.log(data);
+	
+	var pmgwgoals = data.stats[0].splits[0].stat.gameWinningGoals;
+
+		$('.pmgwgoals').append(pmgwgoals);
+	
+	}
+		 );
