6944678146f9eaf5f1d34772253d1d1536de1a49
brad604 <59180282+brad604@users.noreply.github.com>
2021-11-01 12:03:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,15 +9,19 @@ $.getJSON("https://ovirecords.herokuapp.com/https://records.nhl.com/site/api/sco
 	var fortygoalbehind = 12 - data.data[0].fortyGoalSeasons + 1;  
     var fiftygoal = data.data[0].fiftyGoalSeasons;     
     var fiftygoalbehind = 9 - data.data[0].fiftyGoalSeasons + 1;     
-	    var thirtygoal = data.data[0].thirtyGoalSeasons;     
-    var thirtygoalbehind = 9 - data.data[0].thirtyGoalSeasons + 1;     
+	var thirtygoal = data.data[0].thirtyGoalSeasons;     
+    var thirtygoalbehind = 17 - data.data[0].thirtyGoalSeasons + 1;     
+	var twentygoal = data.data[0].twentyGoalSeasons;     
+    var twentygoalbehind = 22 - data.data[0].twentyGoalSeasons + 1;     
 
         $('.fortygoal').append(fortygoal);
         $('.fortygoalbehind').append(fortygoalbehind);
         $('.fiftygoal').append(fiftygoal);
         $('.fiftygoalbehind').append(fiftygoalbehind);	
-	        $('.thirtygoal').append(thirtygoal);
-        $('.thirtygoalbehind').append(thirtygoalbehind);	
+	$('.thirtygoal').append(thirtygoal);
+        $('.thirtygoalbehind').append(thirtygoalbehind);
+	$('.twentygoal').append(twentygoal);
+        $('.twentygoalbehind').append(twentygoalbehind);	
 	
     }
          );
